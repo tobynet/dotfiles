ac37b58ed9a3b4962d69bcfc57f4147cabd95725
tobynet <toby.net.info.mail+git@gmail.com>
2015-12-31 19:36:15 +0900

    Improve a setup script for Linux

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -ex
-files=( .gitconfig .gvimrc .tigrc .tmux.conf .vimrc .zshrc )
+files=( .gitconfig .gvimrc .tigrc .tmux.conf .vimrc .zshrc .ghci )
+
+umask g-w,o-w
 
 # Create symlinks
 pushd ~
@@ -13,4 +15,17 @@ pushd ~
     ln -s --force "dotfiles/$file" .
   done
 popd
+
+# Get oh-my-zsh to ~/.oh-my-zsh/
+# refs. https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh
+if [[ ! -d ~/.oh-my-zsh/ ]]; then
+    echo Getting oh-my-zsh to ~/.oh-my-zsh/
+    git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+    echo Finished to get oh-my-zsh
+else
+    echo oh-my-zsh is already installed!!
+fi
+
+# Show notes
+echo run "chsh -s /usr/bin/zsh" if you use zsh
 
